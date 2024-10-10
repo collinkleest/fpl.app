0ee31082d16f41af2a3a1d71f92c9249b7f8f232
Collin Kleest <collinkleest@gmail.com>
2024-10-09 23:25:59 -0400

    feat: use custom cors proxy server

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,4 +1,4 @@
-export const CORS_PROXY = 'https://corsproxy.io'
+export const CORS_PROXY = 'http://liteproxy.collinkleest.com/proxy'
 export const FPL_API = 'https://fantasy.premierleague.com'
-export const FPL_CORS_ENDPOINT = `${CORS_PROXY}?${FPL_API}`
+export const FPL_CORS_ENDPOINT = `${CORS_PROXY}?url=${FPL_API}`
 export const RAMS_LEAGUE_ID = '178697'
