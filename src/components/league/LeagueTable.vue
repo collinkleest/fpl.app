da95d44c723db6dd78dd5a3b2ccb89eded11e63f
Collin Kleest <collinkleest@gmail.com>
2025-02-06 00:29:22 -0500

    feat: update league table

--- src/components/league/LeagueTable.vue
+++ src/components/league/LeagueTable.vue
@@ -1,6 +1,24 @@
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse; /* Ensures borders are clean */
+  border-spacing: 0; /* Removes extra spacing */
+}
+
+th, td {
+  padding: 10px;
+  text-align: left;
+  border: 1px solid var(--vt-c-text-dark-2); /* Subtle line between rows */
+}
+
+
+</style>
+
 <script setup lang="ts">
 import { useRamsStore } from '@/stores/rams'
+import { computed } from 'vue'
 const ramsStore = useRamsStore()
+const results = computed(() => ramsStore.leagueData?.standings?.results || [])
 </script>
 
 <template>
@@ -12,15 +30,19 @@ const ramsStore = useRamsStore()
         <th>Player Name</th>
         <th>GW Points</th>
         <th>Total Points</th>
+        <th>Points till 🐸</th>
+        <th>Points till 🔝</th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="player in ramsStore?.leagueData?.standings?.results" v-bind:key="player.id">
+      <tr v-for="(player, index) in results" v-bind:key="player.id">
         <td>{{ player.rank }}</td>
         <td>{{ player.entry_name }}</td>
-        <td>{{ player.player_name }}</td>
+        <td>{{ player.player_name.toLowerCase() == "vincent delisi" ? "Vincent Wanker DeLisi" : player.player_name }}</td>
         <td>{{ player.event_total }}</td>
         <td>{{ player.total }}</td>
+        <td>{{ index === 0 ? 0 : results[index - 1]?.total - player.total}}</td>
+        <td>{{ index === 0 ? 0 : results[0]?.total - player.total}}</td>
       </tr>
     </tbody>
   </table>
